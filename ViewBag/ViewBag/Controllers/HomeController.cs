c556b2c29b94f6ba777195ec59ad7dfcef609f4e
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-19 22:08:34 -0300

    ViewBag

---
+++ ViewBag/ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewBag.Models;
+
+namespace ViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            IList<Produto> produtos = new List<Produto>();
+
+            produtos.Add(new Produto { Nome = "Café", Preco = 3.75 });
+            produtos.Add(new Produto { Nome = "Leite", Preco = 1.75 });
+            produtos.Add(new Produto { Nome = "Açucar", Preco = 5.25 });
+
+            ViewBag.produtos = produtos;
+
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
